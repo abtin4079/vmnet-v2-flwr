ff14c093483c2040ff1ebac37391cc67a12fdf31
abtin4079 <abtin.aptitude@gmail.com>
2024-06-10 00:10:01 +0330

    fix

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main(cfg: DictConfig):
         num_clients= cfg.num_clients,
         config = fl.server.ServerConfig(num_rounds=cfg.num_rounds), 
         strategy= strategy,
-        client_resources = {'num_cpus': 2.0, "num_gpus": 0.5},
+        client_resources = {'num_cpus': 2.0, "num_gpus": 1.0},
     )
 
     ##6. save your results
